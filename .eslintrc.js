0f631757a3fdab763f7bdbc9776689fd89a85f31
mattiapomelli <mattiapomelli@gmail.com>
2021-01-12 16:59:46 +0100

    warn prettier instead of error

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'prettier/prettier': [
-      'error',
+      'warn',
       {
         endOfLine: 'auto',
       },
