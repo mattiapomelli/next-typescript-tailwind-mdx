99b998af5bc41d5f218e2950dab63bf6bc82da12
mattiapomelli <mattiapomelli@gmail.com>
2021-03-18 22:23:20 +0100

    fixed navbar and main padding

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,12 +5,10 @@ import DarkModeToggle from './dark-mode-toggle'
 
 const Navbar: FunctionComponent = () => {
   return (
-    <header className="flex justify-between items-center w-full max-w-3xl mx-auto px-8 md:px-10 py-6">
+    <header className="flex justify-between items-center w-full max-w-3xl mx-auto px-4 md:px-10 py-6">
       <Link href="/">
         <a>
-          <h1 className="font-black text-xl text-main-blue dark:text-main-light-blue">
-            MyApp
-          </h1>
+          <h1 className="font-black text-xl">MyApp</h1>
         </a>
       </Link>
       <nav className="flex items-center">
--- layouts/index.tsx
+++ layouts/index.tsx
@@ -6,7 +6,7 @@ const DefaultLayout: FunctionComponent = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main className="w-full max-w-3xl mx-auto px-8 md:px-10">{children}</main>
+      <main className="w-full max-w-3xl mx-auto px-4 md:px-10">{children}</main>
     </>
   )
 }
